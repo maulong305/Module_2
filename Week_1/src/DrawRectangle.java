8af614097813d03a1b71f152af48cb0ea2f103b7
long <maulong305@gmail.com>
2020-06-10 09:48:30 +0700

    week1 day 1,2,3

---
+++ Week_1/src/DrawRectangle.java
@@ -0,0 +1,10 @@
+public class DrawRectangle {
+    public static void main(String[] args) {
+        for (int i = 0; i < 7; i++) {
+            System.out.print("\n");
+            for (int j = 0; j < 5; j++) {
+                System.out.print("  *  ");
+            }
+        }
+    }
+}
---
+++ Week_1/src/DrawTriangle3.java
@@ -0,0 +1,5 @@
+public class DrawTriangle3 {
+    public static void main(String[] args) {
+
+        }
+    }
