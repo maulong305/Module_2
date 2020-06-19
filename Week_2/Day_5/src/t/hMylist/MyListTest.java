9d7c4bf9c29ae92550387203194761059eb55480
long <maulong305@gmail.com>
2020-06-19 15:58:08 +0700

    bài thực hành kiểm thử

---
+++ Week_2/Day_5/src/t/hMylist/MyListTest.java
@@ -0,0 +1,20 @@
+package Day_5;
+
+public class MyListTest {
+    public static void main(String[] args) {
+        MyList<Integer> listInterger = new MyList<Integer>();
+        listInterger.add(1);
+        listInterger.add(2);
+        listInterger.add(3);
+        listInterger.add(3);
+        listInterger.add(4);
+
+        System.out.println("element 4: " + listInterger.get(4));
+        System.out.println("element 1: " + listInterger.get(1));
+        System.out.println("element 2: " + listInterger.get(2));
+
+        listInterger.add(-1);
+        System.out.println("element -1: " + listInterger.get(-1));
+
+    }
+}
