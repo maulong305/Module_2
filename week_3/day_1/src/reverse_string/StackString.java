f5d875a56016ed9d462b3f63817b157fb439157e
long <maulong305@gmail.com>
2020-06-23 23:53:49 +0700

    bài tập đảo ngược phần tử chuỗi dùng Stack

--- week_3/day_1/src/reverse_string/StackString.java
+++ week_3/day_1/src/reverse_string/StackString.java
@@ -1,2 +1,47 @@
-package reverse_string;public class StackString {
+package reverse_string;
+
+public class StackString {
+//    khai báo thuộc tính
+    private String arr[];
+    private int size;
+    private int index = 0;
+//    phương thức khởi tạo
+    public StackString(int size) {
+        this.size = size;
+        arr = new String[size];
+    }
+//    cài đặt phương thức empty
+    public boolean isEmpty() {
+        if (index == 0) {
+            return true;
+        }
+        return false;
+    }
+//    cài đặt phương thức full
+    public boolean isFull() {
+        if (index == size) {
+            return true;
+        }
+        return false;
+    }
+//    cài đặt phương thức push
+    public void push(String element) {
+        if (isFull()) {
+            throw new StackOverflowError("Stack full, can not add element");
+        }
+        arr[index] = element;
+        index++;
+    }
+//    cài đặt phương thức pop
+    public String pop() throws Exception {
+        if (isEmpty()) {
+            throw new  Exception("Stack is null");
+        }
+        return arr[--index];
+    }
+//    cài đặt phương thức size
+    public int size() {
+        return index;
+    }
+
 }
--- week_3/day_1/src/reverse_string/TesReverseString.java
+++ week_3/day_1/src/reverse_string/TesReverseString.java
@@ -1,2 +1,23 @@
-package reverse_string;public class TesReverseString {
+package reverse_string;
+
+public class TesReverseString {
+    public static void main(String[] args) throws Exception {
+        String arrString[] = new String[]{"long", "Vinh", "Hiếu", "Thành" };
+
+        for (String i: arrString){
+            System.out.print(i + "\t");
+        }
+        System.out.println("\n");
+
+        StackString stackString = new StackString(4);
+        for (String i : arrString){
+            stackString.push(i);
+        }
+        for (int i = 0; i < arrString.length; i++){
+            arrString[i] = stackString.pop();
+        }
+        for (String i : arrString) {
+            System.out.print(i + "\t");
+        }
+    }
 }
