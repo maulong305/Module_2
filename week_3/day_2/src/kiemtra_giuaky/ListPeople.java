1136c1f253a8ce96f7038d7b1425bb25b7243a64
long <maulong305@gmail.com>
2020-06-23 23:18:07 +0700

    kiểm tra giữa kỳ: hiển thị, thêm, xóa phần tử trong ArrayList

--- week_3/day_2/src/kiemtra_giuaky/ListPeople.java
+++ week_3/day_2/src/kiemtra_giuaky/ListPeople.java
@@ -1,2 +1,40 @@
-package kiemtra_giuaky;public class ListPeople {
+package kiemtra_giuaky;
+
+public class ListPeople<S> {
+//    khai báo thuộc tính tên, tuổi
+    private String name;
+    private int age;
+    private int id =0;
+    static int count = 0;
+
+//    phương thức khởi tạo có tham số
+    public ListPeople(String name, int age) {
+        this.age = age;
+        this.name = name;
+        this.id = count;
+        count++;
+
+    }
+
+    public ListPeople() {
+
+    }
+//    Cài đặt phương thức
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    public int getId() { return id; }
+
+    @Override
+    public String toString() {
+        return  " id: " + getId() +
+                " name: " + getName() +
+                " , age: " + getAge();
+    }
 }
--- week_3/day_2/src/kiemtra_giuaky/TestListPeople.java
+++ week_3/day_2/src/kiemtra_giuaky/TestListPeople.java
@@ -1,2 +1,84 @@
-package kiemtra_giuaky;public class TestListPeople {
+package kiemtra_giuaky;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TestListPeople {
+    private static ArrayList<ListPeople> listPeople = new ArrayList<>();
+    public static void main(String[] args) {
+        listPeople.add(new ListPeople("Long",33));
+        listPeople.add(new ListPeople("Vinh",20));
+        listPeople.add(new ListPeople("Tuyển",28));
+        listPeople.add(new ListPeople("Thịnh",30));
+        listPeople.add(new ListPeople("Thắng",25));
+
+        System.out.println("Menu : ");
+        System.out.println("Option 1 : Show ListPeople ");
+        System.out.println("Option 2 : Add element in ListPeople");
+        System.out.println("Option 3 : Remove element");
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            int n = scanner.nextInt();
+            switch (n) {
+//                hiện danh sách
+                case 1:
+                    displays();
+                    break;
+
+//                    thêm vào danh sách
+                case 2:
+                    System.out.println("choose add method ");
+                    System.out.println("Option 1 : Add last");
+                    System.out.println("Option 2 : Add index");
+                    int m = scanner.nextInt();
+                    if (m == 1) {
+                        System.out.println("Enter name : ");
+                        String name1 = scanner.next();
+                        System.out.println("Enter age : ");
+                        int age1 = scanner.nextInt();
+                        listPeople.add(new ListPeople(name1, age1));
+                        displays();
+
+                    }
+                    if (m == 2) {
+                        System.out.println("Enter name : ");
+                        String name1 = scanner.next();
+                        System.out.println("Enter age : ");
+                        int age1 = scanner.nextInt();
+                        System.out.println("Enter id : ");
+                        int index = scanner.nextInt();
+                        listPeople.add(index, new ListPeople(name1, age1));
+                        displays();
+                    }
+                    break;
+//                    xóa khỏi danh sách
+
+                case 3:
+                    System.out.println("Enter id");
+                    int id = scanner.nextInt();
+                    for (int j = 0; j < listPeople.size(); j++) {
+                        if (id == listPeople.get(j).getId()) {
+
+                            listPeople.remove(j);
+                            break;
+                        }
+                    }
+                    displays();
+                    break;
+                case 0:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Enter mumber again:");
+            }
+        }
+    }
+    public static void displays(){
+        System.out.println("ListPeople : ");
+        for (int i=0; i< listPeople.size(); i++){
+            System.out.println("id : " + listPeople.get(i).getId() + ", Name: "
+                    +listPeople.get(i).getName()+" ,Age: "+ listPeople.get(i).getAge());
+        }
+
+    }
 }
